a2b8f69ffb2d7fd94a0daa38cda68f1103984e13
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2024-05-31 12:17:11 -0300

    initial setup

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">aora!</Text>
+      <Link href="/profile" style={{color: 'blue'}}>Ir para o perfil</Link>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
