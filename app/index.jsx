71725e56aefd805b6604a389386974ddd5f7f8f9
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2024-06-03 23:04:32 -0300

    alterado nome da página inicial

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">aora!</Text>
+      <Text className="text-3xl">hello world</Text>
       <Link href="/profile" style={{color: 'blue'}}>Ir para o perfil</Link>
       <StatusBar style="auto" />
     </View>
